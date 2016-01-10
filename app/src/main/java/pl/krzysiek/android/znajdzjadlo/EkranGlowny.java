09bc9dc8b707e4b0ee9db7ea214cd665d5cdf378
Rafal Raczkowski <raczkowskir@yahoo.com>
2016-01-10 16:31:52 +0100

    Adding the localization hint

--- app/src/main/java/pl/krzysiek/android/znajdzjadlo/EkranGlowny.java
+++ app/src/main/java/pl/krzysiek/android/znajdzjadlo/EkranGlowny.java
@@ -6,19 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import android.util.Log;
-
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import android.os.Bundle;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+public class EkranGlowny extends Activity implements LocationListener {
 
 
-public class EkranGlowny extends Activity {
 
     private Button btnSearch; //definicja buttona
     private EditText etPlace; //definicja edit text miejscowosc
     private Spinner etDish; //definicja edit text danie
+
+    LocationManager lm;
+    Criteria kr;
+    Location loc;
+    String najlepszyDostawca;
+
+    private void odswiez() {
+        najlepszyDostawca = lm.getBestProvider(kr, true);
+        loc = lm.getLastKnownLocation(najlepszyDostawca);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +71,58 @@ public class EkranGlowny extends Activity {
 
         });
 
+        kr = new Criteria();
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        odswiez();
+        lm.requestLocationUpdates(najlepszyDostawca, 1000, 1, this);
+
+        LocationAddress locationAddress = new LocationAddress();
+        locationAddress.getAddressFromLocation(loc.getLatitude(), loc.getLongitude(),
+                getApplicationContext(), new GeocoderHandler());
 
 
     }
 
+
+
+
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private class GeocoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            etPlace.setText(locationAddress);
+        }
+    }
 }
